1d702ec235397ef628f53915bd26307ebb31486e
Christian Zollman <cmzollman@cmz.local>
2021-12-20 10:32:48 -0800

    Post Build update

---
+++ rust_demo/target/rls/debug/deps/rust_demo-cb08f1d0605d9d3d.d
@@ -0,0 +1,5 @@
+p:\projects\hello_world\rust_demo\rust_demo\target\rls\debug\deps\rust_demo-cb08f1d0605d9d3d.rmeta: src\main.rs
+
+p:\projects\hello_world\rust_demo\rust_demo\target\rls\debug\deps\rust_demo-cb08f1d0605d9d3d.d: src\main.rs
+
+src\main.rs:
---
+++ rust_demo/target/rls/debug/deps/rust_demo-d79932b740936f3d.d
@@ -0,0 +1,5 @@
+p:\projects\hello_world\rust_demo\rust_demo\target\rls\debug\deps\rust_demo-d79932b740936f3d.rmeta: src\main.rs
+
+p:\projects\hello_world\rust_demo\rust_demo\target\rls\debug\deps\rust_demo-d79932b740936f3d.d: src\main.rs
+
+src\main.rs:
