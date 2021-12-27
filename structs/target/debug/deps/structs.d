3d9b669d1904ade2a325209012f0ca9d94330988
Christian Zollman <cmzollman@cmz.local>
2021-12-27 11:23:03 -0800

    Update code structs

---
+++ structs/target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+P:\projects\hello_world\rust_demo\structs\target\debug\deps\structs.exe: src\main.rs
+
+P:\projects\hello_world\rust_demo\structs\target\debug\deps\structs.d: src\main.rs
+
+src\main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+P:\projects\hello_world\rust_demo\structs\target\debug\structs.exe: P:\projects\hello_world\rust_demo\structs\src\main.rs
